25af5c187809c595160892022bb5c15266b81bb7
manojsai7 <144029171+manojsai7@users.noreply.github.com>
2023-09-13 13:08:32 +0530

    removed Fsub -L53

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def approve(_, m : Message):
 @app.on_message(filters.command("start"))
 async def op(_, m :Message):
     try:
-        await app.get_chat_member(cfg.CHID, m.from_user.id) 
+#        await app.get_chat_member(cfg.CHID, m.from_user.id) 
         if m.chat.type == enums.ChatType.PRIVATE:
             keyboard = InlineKeyboardMarkup(
                 [
